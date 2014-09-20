8b5dd09b5f84e4347ed5d561c6633fdf57c50032
kunalp320 <kunalp320@gmail.com>
2014-09-20 16:45:52 -0400

    Moved javascript code into a map.js file

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,10 +1,46 @@
 $( document ).ready(function() {
-  var mapOptions = {
-    zoom: 8,
-    center: new google.maps.LatLng(-34.397, 150.644)
-  };
+var shapes_added_so_far = [];
 
-  var map = new google.maps.Map(document.getElementById('map_canvas'),
+  $('.clear_button').click(function () {
+    for(var i = 0; i < shapes_added_so_far.length; i++) {
+      var item_to_clear = shapes_added_so_far[i];
+      item_to_clear.setMap(null);  
+    }
+    clear_all_shapes = [];
+  });
+
+  function initialize() {
+    var mapOptions = {
+      center: new google.maps.LatLng(-34.397, 150.644),
+      zoom: 8
+    };
+
+    var map = new google.maps.Map(document.getElementById('map_canvas'),
       mapOptions);
-  
+
+    var drawingManager = new google.maps.drawing.DrawingManager({
+      drawingMode: google.maps.drawing.OverlayType.MARKER,
+      drawingControl: true,
+      drawingControlOptions: {
+        position: google.maps.ControlPosition.TOP_CENTER,
+        drawingModes: [
+          google.maps.drawing.OverlayType.RECTANGLE
+        ]
+      },
+      rectangleOptions: {
+        editable: true
+      }
+    });
+
+    google.maps.event.addListener(drawingManager, 'overlaycomplete', function(event) {
+      if(event.type == google.maps.drawing.OverlayType.RECTANGLE) {
+        var shape_added = event.overlay;
+        shapes_added_so_far.push(shape_added);
+        shape_added.setEditable(false);
+      }
+    });
+
+    drawingManager.setMap(map);
+  }
+  initialize();
 });
