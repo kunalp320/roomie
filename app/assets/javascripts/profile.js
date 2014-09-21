68ee2739c8783bd7fbd8458424a752f895cabd51
Suren Nihalani <suren.k.n@me.com>
2014-09-21 04:17:42 -0400

    IT WORKS

--- app/assets/javascripts/profile.js
+++ app/assets/javascripts/profile.js
@@ -10,8 +10,24 @@ $( document ).ready(function() {
     type: "GET",
     url: "/get_list",
     success: function(data) {
+      for(var i = 0; i < data.length; i++) {
+        var rect = data[i];
+        var a = rect['n_lat'];
+        var b = rect['n_long'];
+
+        var c = rect['s_lat'];
+        var d = rect['s_long'];
+        var rectangle = new google.maps.Rectangle({
+          map: map,
+          bounds: new google.maps.LatLngBounds(
+            new google.maps.LatLng(a, b),
+            new google.maps.LatLng(c, d))
+        }); 
+
+      }
       console.log(data);
       console.log('lol');
+
       alert("success")
     },
     error: function(data) {
