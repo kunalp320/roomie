5030d14479d76b0ee62a215b59767ce3ae2b0213
Suren Nihalani <suren.k.n@me.com>
2014-09-21 00:57:58 -0400

    Sending data for ajax now

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,6 +1,32 @@
 $( document ).ready(function() {
 var shapes_added_so_far = [];
 
+  $('.submit_profile').click(
+    function() {
+      var all_locations = []
+      for(var i = 0; i < shapes_added_so_far.length; i++) {
+        var shape_to_send = shapes_added_so_far[i];
+        var ne = shape_to_send.getBounds().getNorthEast();
+        var sw = shape_to_send.getBounds().getSouthWest();
+        all_locations.push([[ne.lat(), ne.lng()], [sw.lat(), sw.lng()]]);
+      }
+      $.ajax({
+        type: "GET",
+        url: '/save_prefs',
+        data: { 
+          'here': all_locations,
+          'length': all_locations.length
+        },
+        success: function(data) {
+          alert("hello");
+        },
+        error: function(data) {
+          alert(data);
+        }
+      });
+    }
+  );
+
   $('.clear_button').click(function () {
     for(var i = 0; i < shapes_added_so_far.length; i++) {
       var item_to_clear = shapes_added_so_far[i];
@@ -35,17 +61,7 @@ var shapes_added_so_far = [];
     google.maps.event.addListener(drawingManager, 'overlaycomplete', function(event) {
       if(event.type == google.maps.drawing.OverlayType.RECTANGLE) {
         var shape_added = event.overlay;
-        $.ajax({
-          type: "GET",
-          url: '/save_prefs',
-          data: { 'here': 'lol'},
-          success: function(data) {
-            alert("hello")
-          },
-          error: function(data) {
-            alert(data)
-          }
-        });
+        console.log(shape_added.getBounds().getNorthEast());
         shapes_added_so_far.push(shape_added);
         shape_added.setEditable(false);
         drawingManager.setDrawingMode(null);
